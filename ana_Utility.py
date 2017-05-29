0b7a3985e9788af3391c7af3715e3e6c0dee0bbe
Hironobu Takahashi <tbkbhbsb@gmail.com>
2017-05-30 01:07:18 +0900

    2017/05/30 commit

--- ana_Utility.py
+++ ana_Utility.py
@@ -8,8 +8,43 @@ def get_record_resampling_month_data_at_year(df, resample_tar_col, sum_col):
     return add_df.resample('M', on=resample_tar_col).sum()
 
 
+def sprit_YMD(df, dateColName):
+
+    sprited = df.copy()
+    print(sprited.index)
+
+    # print(df[])
+
+    # y = lambda x: x[dateColName].year
+    # m = lambda x: x[dateColName].month
+    # d = lambda x: x[dateColName].day
+
+    # df["year"] = df.apply(y, axis=1)
+    # sprited["month"] = df.apply(m, axis=1)
+    # sprited["day"] = df.apply(d, axis=1)
+
+    return sprited
+
+
+def sprit_YMD_index(df):
+    sprited = df.copy()
+
+    for index, time in enumerate(sprited.index):
+        print(type(index))
+        print(sprited[index])
+        # sprited[index]["year"] = index.year
+        # sprited[index]["month"] = index.month
+        # sprited[index]["day"] = index.day
+
+    print(sprited)
+
+    return sprited
+
+
 if __name__ == '__main__':
     for df in read_excel("ブック1.xlsx"):
         resampling_data = get_record_resampling_month_data_at_year(
             df, "作成日", len(df.columns))
-        df_to_csv(resampling_data, "test.csv")
+        # print(resampling_data)
+        sprit_YMD_index(resampling_data)
+        # df_to_csv(resampling_data, "test.csv")
