16e5ae36b8cb301c8bcaa0e65ab32fc108dce4a0
takahashihrzg <takahashihrhrzg@nttdata.co.jp>
2017-06-02 20:01:41 +0900

    2017/06/02

--- ana_Utility.py
+++ ana_Utility.py
@@ -8,6 +8,10 @@ def get_record_resampling_month_data_at_year(df, resample_tar_col, sum_col):
     return add_df.resample('M', on=resample_tar_col).sum()
 
 
+def get_record_resampling_business_year_end_frequency(df, resample_tar_col, sum_col):
+    add_df = add_col_ticketCounter(df)
+    return add_df.resample('A', on=resample_tar_col).sum()
+
 def sprit_YMD(df, dateColName):
 
     sprited = df.copy()
@@ -15,36 +19,27 @@ def sprit_YMD(df, dateColName):
 
     # print(df[])
 
-    # y = lambda x: x[dateColName].year
-    # m = lambda x: x[dateColName].month
-    # d = lambda x: x[dateColName].day
+    y = lambda x: x[dateColName].year
+    m = lambda x: x[dateColName].month
+    d = lambda x: x[dateColName].day
 
-    # df["year"] = df.apply(y, axis=1)
-    # sprited["month"] = df.apply(m, axis=1)
-    # sprited["day"] = df.apply(d, axis=1)
+    sprited["year"] = df.apply(y, axis=1)
+    sprited["month"] = df.apply(m, axis=1)
+    sprited["day"] = df.apply(d, axis=1)
 
     return sprited
 
 
 def sprit_YMD_index(df):
     sprited = df.copy()
-
-    for index, time in enumerate(sprited.index):
-        print(type(index))
-        print(sprited[index])
-        # sprited[index]["year"] = index.year
-        # sprited[index]["month"] = index.month
-        # sprited[index]["day"] = index.day
-
-    print(sprited)
-
-    return sprited
+    sprited["y-m-d"] = sprited.index
+    return sprit_YMD(sprited, "y-m-d")
 
 
 if __name__ == '__main__':
-    for df in read_excel("ブック1.xlsx"):
-        resampling_data = get_record_resampling_month_data_at_year(
-            df, "作成日", len(df.columns))
-        # print(resampling_data)
-        sprit_YMD_index(resampling_data)
-        # df_to_csv(resampling_data, "test.csv")
+    for df in read_excel("案件一覧_201705291850.xlsx"):
+        resampling_data = get_record_resampling_business_year_end_frequency(
+            df, "プロジェクト開始年月日", len(df.columns))
+        YMD_splited = sprit_YMD_index(resampling_data)
+        # print(YMD_splited)
+        df_to_csv(YMD_splited, "プロジェクト開始年集計_年度推移.csv")
